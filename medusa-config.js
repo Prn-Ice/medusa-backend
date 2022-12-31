19a947bda218fdcb6560a3f28a6311599be961b0
Prn-Ice <stormprince77@gmail.com>
2022-12-31 14:44:07 +0100

    fix: add proper module exports, remove compose file

--- medusa-config.js
+++ medusa-config.js
@@ -26,7 +26,7 @@ try {
 const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8100,http://localhost:8000,http://storefront:8100";
 
 // Database URL (here we use a local database called medusa-development)
 const DATABASE_URL =
@@ -56,12 +56,10 @@ const plugins = [
 
 module.exports = {
   projectConfig: {
-    // redis_url: REDIS_URL,
+    redis_url: REDIS_URL,
     // For more production-like environment install PostgresQL
-    // database_url: DATABASE_URL,
-    // database_type: "postgres",
-    database_database: "./medusa-db.sql",
-    database_type: "sqlite",
+    database_url: DATABASE_URL,
+    database_type: "postgres",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
   },
